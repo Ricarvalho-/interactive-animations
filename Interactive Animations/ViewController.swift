8f56515143ce61e3cd5b0ac7d3e8627706cfa028
Ricardo Carvalho <ricardo.carvalho@dextra-sw.com>
2021-03-31 19:39:19 -0300

    Initial Commit

---
+++ Interactive Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Interactive Animations
+//
+//  Created by Ricardo de Carvalho on 31/03/21.
+//  Copyright © 2021 Ricardo de Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
